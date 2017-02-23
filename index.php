de0607d3ba03bcf4f8e48c0984ee8a9c8b47a453
LAPTOP-6I3EU5UJ\Sander van Steenis <99039450@mydavinci.nl>
2017-02-23 11:22:34 +0100

    Update

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="UTF-8">
+	<title>Mad Libs</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" type="image/png" href="img/icon.png"/>
+	<link href="https://fonts.googleapis.com/css?family=Sniglet" rel="stylesheet">
+</head>
+<body>
+
+<h1>Mad Libs</h1>
+
+
+<div class="main">
+<nav>
+<ul>
+  <li><a class="active" href="yo.html">Er heerst paniek...</a></li>
+  <li><a href="yo2.html">Onkunde</a></li>
+</ul>
+</nav>	
+</div>
+
+</body>
+</html>
